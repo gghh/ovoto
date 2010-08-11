21739162a0c7c46e96161f61a341631143f101d3
ab.mancini@gmail.com <ab.mancini@gmail.com@9c8269dc-39f9-ea45-df89-f4cefb97cb27>
2010-08-11 13:03:24 +0000

    Ottimizzati gli imports con C-S-o Cmnd-O

--- trunk/ovoto/src/ovoto/math/unifi/it/client/Ovoto.java
+++ trunk/ovoto/src/ovoto/math/unifi/it/client/Ovoto.java
@@ -16,7 +16,6 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
